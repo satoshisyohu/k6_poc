5653e2a5bcb09533d404016f439f16d3bac26ee6
satoshisyohu <satoshisyohu@gmail.com>
2023-11-06 21:57:15 +0900

    this is first commit

---
+++ poc_k6.js
@@ -0,0 +1,39 @@
+import http from 'k6/http';
+import { check } from "k6";
+import * as jwt from 'jsonwebtoken';
+
+
+const LOCAL_URL = 'http://localhost:8080/satoshisyohu/todos/retrieve'
+
+export default function () {
+    const jwtPayload = {
+        email: 'user1@example.com',
+        name: 'JWT Taro',
+    };
+    const jwtSecret = 'secret_key_goes_here';
+    const jwtOptions = {
+        algorithm: 'HS256',
+        expiresIn: '3s',
+    };
+
+    const token = jwt.sign(jwtPayload, jwtSecret, jwtOptions);
+
+
+
+    let data = {
+    status: '1',
+    createdDate: '2023-10-14'
+};
+
+let response = http.post(LOCAL_URL, JSON.stringify(data), {
+    headers: { 
+        'Content-Type': 'application/json',
+        'cookie':'token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2OTkxOTI2NzIsInVzZXJfaWQiOiJiNmQyMTIwNy0wYWVkLTRmNGMtYTk1NS1mMGNjNDZkYzM5NjIifQ.bvoU7n4RxKOlY0AQfmp8N8DOCkjEPubH4Xe3jpQ6wrM; Path=/; Domain=localhost; Max-Age=10; HttpOnly'},
+  });
+  console.log(data)
+
+  check(response, {
+    "status was 200": (r) => r.status === 200
+});
+
+}
